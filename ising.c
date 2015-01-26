67eb1a6d389521a20462f63b6fa4c46dc17f74fc
r-hartmann <r-hartmann@freenet.de>
2015-01-26 21:19:32 +0100

    usage neu(required, reihenfolge, neue parameter)
    parameter  da aber noch nicht implementiert.

--- ising.c
+++ ising.c
@@ -13,34 +13,40 @@
 
 static void usage(char* progname) // typical usage-function
 {
-    printf("\nUsage: %s [N] (steps) (calcMode) (filmMode) (sweepMode) (S_i) (S_e) (S_s) (K)\n", progname);
+    printf("\nUsage: %s [N] [dim] (steps) (sweepVar) (S_i) (S_e) (S_s) (K) (sweepMode) (hystMode) (calcMode) (filmMode)\n", progname);
     printf("Where values in [] are required, while values in () are optional \n\n");
-    printf("  - N: Size of Matrix ( int value > 0 ) \n");
+    printf("  - N: length of Matrix in one dimesion ( int value > 0 ) \n");
+    printf("  - dim: Dimension of Matrix ( int value = 2 or 3 ) \n");
     printf("  - steps: Number of MonteCarlo steps ( int value > 0 ) \n");
-    printf("  - calcMode: energy calculating via nearest neighbour or meanfield aproximation ( n or m ) \n");
-    printf("  - filmMode: additional lattice saving ( y or n ) \n");
-    printf("  - sweepMode: montecarlo-simulations for differnt magnetic fields or temperatures( B or T ) \n");
+    printf("  - sweepPar: sweep parameter for montecarlo-simulations ( magnetic field  or temperatures )( B or T ) \n");
     printf("  - S_i: initial value of sweep parameter for the first montecarlo-simulation ( double value ) \n");
     printf("  - S_e: value of sweep parameter where the Simualtion will end ( double value ) \n");
     printf("  - S_s: step size of sweep parameter ( double value ) \n");
-    printf("  - C: is the extern constant parameter over the sweep ( double value ) \n");
+    printf("  - C: is the value of extern constant parameter over the sweep ( double value ) \n");
+    printf("  - sweepMode: sweep over one lattice or new lattice for every sweep step ( y or n ) \n");
+    printf("  - hystMode: additional simulation back from S_e to S_i ( y or n ) \n");
+    printf("  - calcMode: energy calculating via nearest neighbour or meanfield aproximation ( n or m ) \n");
+    printf("  - filmMode: additional lattice saving ( y or n ) \n");
     printf("\n");
-    printf("Example: %s 25 1000 n y T 1.50 4.00 0.10 0.00 \n", progname);
+    printf("Example: %s 25 2 1000 T 1.50 4.00 0.10 0.00 n n n n \n", progname);
     exit(EXIT_FAILURE);
 }
 
-void getParameters(int argc, char **argv, int *N, int *steps, char *calcMode, char *filmMode, char *sweepMode, double *sweep_init, double *sweep_end, double *sweep_step, double *C)
+void getParameters(int argc, char **argv, int *N, int *dim, int *steps, char *sweepPar, double *sweep_init, double *sweep_end, double *sweep_step, double *C, char *sweepMode, char *hystMode, char *calcMode, char *filmMode)
 {
-    if(argc !=10 
+    if(argc < 3 
       || sscanf(argv[1], "%d", N) != 1 || *N < 1  
-      || sscanf(argv[2], "%d", steps) != 1 || *steps < 1 
-      || sscanf(argv[3], "%c", calcMode) != 1 || (*calcMode != 'n' && *calcMode != 'm')
-      || sscanf(argv[4], "%c", filmMode) != 1 || (*filmMode != 'y' && *filmMode != 'n')
-      || sscanf(argv[5], "%c", sweepMode) != 1 || (*sweepMode != 'B' && *sweepMode != 'T')
-      || sscanf(argv[6], "%lf", sweep_init) != 1
-      || sscanf(argv[7], "%lf", sweep_end) != 1
-      || sscanf(argv[8], "%lf", sweep_step) != 1
-      || sscanf(argv[9], "%lf", C) != 1) 
+      || sscanf(argv[2], "%d", dim) != 1 || (*dim != 2 && *dim != 3)
+      || (argc > 3 && sscanf(argv[3], "%d", steps) != 1) || *steps < 1
+      || (argc > 4 && sscanf(argv[4], "%c", sweepPar) != 1) || (*sweepPar != 'B' && *sweepPar != 'T')
+      || (argc > 5 && sscanf(argv[5], "%lf", sweep_init) != 1)
+      || (argc > 6 && sscanf(argv[6], "%lf", sweep_end) != 1)
+      || (argc > 7 && sscanf(argv[7], "%lf", sweep_step) != 1)
+      || (argc > 8 && sscanf(argv[8], "%lf", C) != 1)
+      || (argc > 9 && sscanf(argv[9], "%c", sweepMode) != 1) || (*sweepMode != 'y' && *sweepMode != 'n')
+      || (argc > 10 && sscanf(argv[10], "%c", hystMode) != 1) || (*hystMode != 'y' && *hystMode != 'n')
+      || (argc > 11 && sscanf(argv[11], "%c", calcMode) != 1) || (*calcMode != 'n' && *calcMode != 'm')
+      || (argc > 12 && sscanf(argv[12], "%c", filmMode) != 1) || (*filmMode != 'y' && *filmMode != 'n'))
         usage(argv[0]);     
 }
     
@@ -65,13 +71,13 @@ void initialize(char filmMode)
 
 int main(int argc, char **argv)
 {
-    int N, steps = 1000;
+    int N, dim, steps = 1000;
     double sweep_init = 1.5, sweep_end = 4.0, sweep_step = 0.1, C = 0.0000;
-    char calcMode = 'n', filmMode = 'n', sweepMode = 'T';
+    char sweepPar = 'T', sweepMode = 'n', hystMode = 'n', calcMode = 'n', filmMode = 'n';
     
-    getParameters(argc, argv, &N, &steps, &calcMode, &filmMode, &sweepMode, &sweep_init, &sweep_end, &sweep_step, &C);    
+    getParameters(argc, argv, &N, &dim, &steps, &sweepPar, &sweep_init, &sweep_end, &sweep_step, &C, &sweepMode, &hystMode, &calcMode, &filmMode);
 
-    printf("\nattempting to execute %s %d %d %c %c %c %f %f %f %f \n\n", argv[0], N, steps, calcMode, filmMode, sweepMode, sweep_init, sweep_end, sweep_step, C);
+    printf("\nattempting to execute %s %d %d %d %c %f %f %f %f %c %c %c %c \n\n", argv[0], N, dim, steps, sweepPar, sweep_init, sweep_end, sweep_step, C, sweepMode, hystMode, calcMode, filmMode);
     
     initialize(filmMode);
 
@@ -84,8 +90,8 @@ int main(int argc, char **argv)
     unsigned long imagecounter = 0, changecounter = 0; // fuer Film     
 
     spins=matrixMallocDim(N, 2);    
-    if(sweepMode == 'T') B = C; //choose the right sweepMode
-    else if(sweepMode == 'B') T = C;
+    if(sweepPar == 'T') B = C; //choose the right sweepPar (set constant)
+    else if(sweepPar == 'B') T = C;
     
     /* a simulation for each sweep increment */
     for(S=sweep_init; S<=sweep_end; S += sweep_step)
@@ -93,8 +99,8 @@ int main(int argc, char **argv)
         matrixRandFillDim(spins,N,2); // fill matrix with random 1 or -1
         spinSum = spinSumDim(spins, N, 2);
         if(calcMode == 'n') edgeSum = edgeSumDim(spins, N, 2);
-        if(sweepMode == 'T') T = S; //choose the right sweepMode
-        else if(sweepMode == 'B') B = S;
+        if(sweepPar == 'T') T = S; //choose the right sweepPar (set variable)
+        else if(sweepPar == 'B') B = S;
         imagecounter = 0;
         
         for(i=0; i<steps; ++i)
@@ -132,8 +138,8 @@ int main(int argc, char **argv)
         sprintf(filename, "output/matrix_T=%f_B=%f_end.txt", T, B);
         matrixPrint2Dfile(spins,N,N, filename);
         writeOutputFFF(T, magPerSpinDim(spins, N, 2), B, MAGPERSPINOUTPUT);
-        if(sweepMode == 'T') printf("=> T = %f finished\n", T);
-        else if(sweepMode == 'B') printf("=> B = %f finished\n", B);
+        if(sweepPar == 'T') printf("=> T = %f finished\n", T);
+        else if(sweepPar == 'B') printf("=> B = %f finished\n", B);
     }
     printf("\n");
     matrixDeleteDim(spins, 2);   
